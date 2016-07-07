2985e107d07cc9be01c88365e99a06ae1f12bef3
gmittal <gautam@mittal.net>
2016-07-07 15:47:27 -0700

    working through that scheme homework

--- SummerHomework.scm
+++ SummerHomework.scm
@@ -4,3 +4,35 @@
 ; Problem 1 (Easy)
 (define (factorial n)
 (if (= n 1) 1 (* (factorial (- n 1)) n)))
+
+(define (remove-elem xs elem)
+    (cond
+      ((null? ll) '())
+      ((eq? (car ll) elem) (cdr ll))
+       (else 
+        (cons (car ll) (remove-elem (cdr ll) elem))))
+    )
+
+; Problem 2 (Medium)
+(define (odds lst)
+  (length (apply append (map (lambda(n)
+         (if (odd? n)
+             (list n)
+             '())) lst)))
+  )
+
+; Problem 3 (Medium)
+(define (filter p lst)
+  (apply append (map (lambda(n)
+         (if (p n)
+             (list n)
+             '())) lst))
+  )
+
+; Problem 4 (Medium Hard)
+(define (reverse lst)
+  (map (lambda(n)
+         (append (cdr lst) (list n))
+         ) lst)
+  )
+       
