39f2ac91e1215d228552713c46920ae676b9a223
Aaron Schultz <aaron@hschultz.net>
2017-01-04 11:44:15 -0800

    Initial java commit -- started mobiles problem.

---
+++ Java Problem Sets/Mobiles/src/weight.java
@@ -0,0 +1,22 @@
+
+public class weight implements structure {
+
+	@Override
+	public boolean isMobile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isWeight() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isBalanced() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
