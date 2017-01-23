e69c6d525fb97329c19d635aa4299800008f367f
gmittal <gautam@mittal.net>
2017-01-22 20:08:10 -0800

    Gautam takes a shot at RandP.

--- Java Problem Sets/RandP/src/Main.java
+++ Java Problem Sets/RandP/src/Main.java
@@ -1,13 +1,11 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		RandP rand = new RandP(10);
+		RandP rand = new RandP(100);
 
-		for (int i = 0; i < 12; i++)
+		for (int i = 0; i < 101; i++)
 			System.out.println(rand.nextInt());
-
 	}
 
 }
