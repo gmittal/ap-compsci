ae0076380c5504136470c2e39c9451bb2559f969
Gautam Mittal <gautam@mittal.net>
2017-01-23 12:40:11 -0800

    Started BST.

---
+++ Java Problem Sets/BinarySearchTree/src/BST.java
@@ -0,0 +1,33 @@
+
+public class BST<T extends Comparable<T>> {
+	private T datum;
+	private BST<T> left;
+	private BST<T> right;
+
+	public BST(T datum) {
+
+	}
+
+	public T getDatum() {
+		return datum;
+	}
+
+	public BST<T> getLeft() {
+		return left;
+	}
+
+	public BST<T> getRight() {
+		return right;
+	}
+
+	public void printTree() {
+
+	}
+
+	public String toString() {
+		return "";
+	}
+
+	private depth()
+
+}
