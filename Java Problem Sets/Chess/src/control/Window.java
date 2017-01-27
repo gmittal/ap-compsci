1cce63462065e7739f64bb53d8fb05695a350fae
Gautam Mittal <gautam@mittal.net>
2017-01-27 14:40:20 -0800

    Add mouse handlers. (#15)

--- Java Problem Sets/Chess/src/control/Window.java
+++ Java Problem Sets/Chess/src/control/Window.java
@@ -2,13 +2,17 @@ package control;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import board.Board;
 
-public class Window extends JPanel {
+public class Window extends JPanel implements ActionListener, MouseListener {
 
 	private Board board;
 
@@ -22,9 +26,57 @@ public class Window extends JPanel {
 		setPreferredSize(new Dimension(512, 512));
 		f.pack();
 		f.setVisible(true);
+
+		/* Mouse input handler */
+		this.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent evt) {
+				int x = evt.getX() / 64;
+				int y = evt.getY() / 64;
+
+				System.out.println(x + ", " + y);
+
+			}
+		});
 	}
 
 	public void paintComponent(Graphics g) {
 		board.draw(g);
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
