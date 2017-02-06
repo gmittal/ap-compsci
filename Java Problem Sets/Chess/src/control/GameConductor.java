2264bef9a623e8ee5484ad7e4570b273ab7dd777
Aaron Schultz <aaron@hschultz.net>
2017-02-06 11:29:27 -0800

    Implemented turns

--- Java Problem Sets/Chess/src/control/GameConductor.java
+++ Java Problem Sets/Chess/src/control/GameConductor.java
@@ -2,8 +2,11 @@ package control;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.HashSet;
 
 import board.Board;
+import board.Cell;
+import pieces.King;
 import pieces.Piece;
 
 public class GameConductor implements MouseListener {
@@ -11,9 +14,11 @@ public class GameConductor implements MouseListener {
 	public Board board;
 	public Piece selectedPiece;
 	public Network network = new Network();
+	public boolean side;
 
 	public GameConductor() {
 		board = Main.board;
+		side = false;
 		network.updateState();
 	}
 
@@ -25,15 +30,34 @@ public class GameConductor implements MouseListener {
 		if (selectedPiece != null && selectedPiece.getPossibleMoves().contains(board.getCell(x, y))) {
 			selectedPiece.move(board.getCell(x, y));
 			selectedPiece = null;
+			side = !side;
+			Main.window.f.setTitle("Chess - " + (side ? "Black's " : "White's ") + "Move");
 			Main.window.repaint();
 			return;
 		}
 
-		selectedPiece = board.getCell(x, y).piece;
+		if (board.getCell(x, y).piece != null && board.getCell(x, y).piece.side == side)
+			selectedPiece = board.getCell(x, y).piece;
 
 		Main.window.repaint();
 		network.sendLocalChange(); // tell the Network that something happened
 
+	}
+
+	public HashSet<Cell> getAllMoves(boolean s) {
+		HashSet<Cell> moves = new HashSet<>();
+
+		for (Cell[] col : board.cells)
+			for (Cell c : col) {
+				if (c.piece != null && c.piece.side == s)
+					if (!(c.piece instanceof King))
+						moves.addAll(c.piece.getPossibleMoves());
+					else {
+						moves.addAll(((King) c.piece).getPossibleMovesWithoutCheck());
+					}
+			}
+
+		return moves;
 	}
 
 	@Override
