9c5cba8209dfe95b0cbca2c79ac9cf8e5dd8fba4
Gautam Mittal <gautam@mittal.net>
2017-02-08 11:18:56 -0800

    Add GET request. (#33)

--- Java Problem Sets/Chess/src/control/Network.java
+++ Java Problem Sets/Chess/src/control/Network.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -46,6 +47,18 @@ public class Network {
 		/*
 		 * Keep pulling the JSON data and listen for a change --> do something
 		 */
+		
+		  StringBuilder result = new StringBuilder();
+	      URL url = new URL(urlToRead);
+	      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	      conn.setRequestMethod("GET");
+	      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	      String line;
+	      while ((line = rd.readLine()) != null) {
+	         result.append(line);
+	      }
+	      rd.close();
+	      System.out.println(result.toString());
 	}
 
 }
