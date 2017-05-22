7de1605d8f2bb8404cc3b81095033ce7c2aa4587
gmittal <gautam@mittal.net>
2017-05-22 12:36:51 -0700

    Added better networking for Chess client.

--- Java Problem Sets/Chess/src/control/Network.java
+++ Java Problem Sets/Chess/src/control/Network.java
@@ -17,19 +17,29 @@ import board.Board;
 
 public class Network {
 	/* Populate these variables on multi-player startup */
-	public static final String HOST = "http://gautam.cc:9000/games";
-	public static int GAME_PIN = 12348;
+	public static final String HOST = "http://gautam.cc";
+	public static int GAME_PIN = 1134;
 
 	private HashMap<String, Integer> ids = new HashMap<>();
 
 	public static ArrayList<String> state;
 	public Board board = Main.board;
 
+	
+	/* Default constructor creates a new game pin for others to join */
 	public Network() {
+		
+		try {
+			GAME_PIN = startNewGame();
+			System.out.println("MULTIPLAYER GAME PIN: "+GAME_PIN);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
 		state = new ArrayList<>();
 
 		try {
-			URL url = new URL(HOST);
+			URL url = new URL(HOST+":"+GAME_PIN+"/games");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("PUT");
 			connection.setDoOutput(true);
@@ -45,13 +55,28 @@ public class Network {
 		}
 
 	}
-
+	
+	/* Constructor joins another game given a game pin */
+	public Network(int pin) {
+		GAME_PIN = pin;
+	}
+	
+	public int startNewGame() throws IOException {
+		URL url = new URL(HOST+":9000/new");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		int r = Integer.parseInt(rd.readLine(), 10);
+		rd.close();
+		return r;
+	}
+	
 	public void sendLocalChange(String move) throws IOException {
 
 		state.add(move);
 		System.out.println(state.toString());
 
-		URL url = new URL(HOST);
+		URL url = new URL(HOST+":"+GAME_PIN+"/games");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("PUT");
 		connection.setDoOutput(true);
@@ -67,7 +92,7 @@ public class Network {
 
 	public ArrayList<String> listenForNetworkChange() throws IOException, JSONException {
 		StringBuilder result = new StringBuilder();
-		URL url = new URL(HOST);
+		URL url = new URL(HOST+":"+GAME_PIN+"/games");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
